cd80369a101f1fc54d1b8ccecea362407cdfde95
shahbaz shamshi <shahbazshamshi2001@gmail.com>
2020-11-12 19:48:09 +0530

    sum of two number in java

---
+++ sum.java
@@ -0,0 +1,12 @@
+public class AddTwoNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5;
+      int  num2 = 15; 
+      int sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of two numbers: "+sum);
+   }
+}
